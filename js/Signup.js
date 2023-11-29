9fc6c863f65b39d57a7e4182adf1a559bf19feb6
WilliamGC05 <wgc4@usca.edu>
2023-11-29 12:42:59 -0500

    Sign Up

--- js/Signup.js
+++ js/Signup.js
@@ -1,6 +1,12 @@
-var firebaseConfig = {
-  
-};
+const firebaseConfig = {
+  apiKey: "AIzaSyAZfXY-ly6rGdCPzYJzvuD71olzP1O6WdQ",
+  authDomain: "database2023-85f28.firebaseapp.com",
+  projectId: "database2023-85f28",
+  storageBucket: "database2023-85f28.appspot.com",
+  messagingSenderId: "37672644668",
+  appId: "1:37672644668:web:57cbeaa9159c964e75d339",
+  measurementId: "G-PZMM403QE2"
+}; 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -9,17 +15,33 @@ $("#signup-form").submit(function(e) {
   e.preventDefault();
   // get the username(email) and password from the form
   // change the following code
-  //var email = ;
-  //var password = ;
+  var username = $('input[name="fullname"]').val();
+  var emailAddress = $('input[name="email"]').val();
+  var password = $('input[name="password"]').val();
+  var confirmPassword = $('input[name="cpassword"]').val();
+  console.log(username, email, password, cpassword);
+
+  // Check if Password and Confirm Password fields are the same, if not, display an error message
+  if (password != confirmPassword) {
+    alert("Passwords do not match.");
+    return;
+  } 
 
   // create a user with email address and password
   firebase
     .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then(user => {
+    .createUserWithEmailAndPassword(emailAddress, password)
+    .then((result) => {
       // Signed in
-      // ...
-      console.log("You are signed up");
+      let user = result.user;
+      console.log(user);
+      user.updateProfile({
+        displayName: username,
+        //photoURL: "https://example.com/jane-q-user/profile.jpg"
+        //email: emailAddress
+        //textarea: "This is my bio"
+      });
+      console.log(user.displayName + "You are signed up");
       window.location.href = "Login.html";
       
     })
